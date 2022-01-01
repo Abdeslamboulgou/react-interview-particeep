d47a7f6621aebe35c6cfa2a30d843b969e17e2d3
Abdeslamboulgou <boulgouslam@gmail.com>
2022-01-01 19:55:36 +0100

    refactor du code

--- src/components/MoviesCard.js
+++ src/components/MoviesCard.js
@@ -9,21 +9,13 @@ function MoviesCard({ films, updateFilms }) {
     // supprimer element avec filter
     if (window.confirm('vous voulez supprimer ce film ?')) {
       const newMovies = films.filter((movie) => movie.id !== item);
-
-      // ici pour verifier si le film est supprimer
-      console.log(newMovies)
-
-      // modifier la cart
       updateFilms(newMovies);
 
     }
-
   }
-
   return (
     <Pagination  RemoveMovie={RemoveMovie}/>
   )
-
 }
 
 export default MoviesCard;
