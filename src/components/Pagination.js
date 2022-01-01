1c14342388797f1831743fe806b328d4da161f7d
Abdeslamboulgou <boulgouslam@gmail.com>
2022-01-01 02:19:28 +0100

    modifie style pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,7 +5,7 @@ import Categories from './Categories'
 import '../styles/pagination.css'
 import '../styles/moviesCard.css'
 
-function Pagination({ toggleDislikeHandler, toggleLikeHandler, RemoveMovie }) {
+function Pagination({ toggleDislikeHandler, toggleLikeHandler, RemoveMovie , dislikes , likes , setLikes ,setDisLikes }) {
 
     const [activeCategory, AfficheMovies] = useState('');
     const categories = movies.reduce((acc, movie) => acc.includes(movie.category) ? acc : acc.concat(movie.category), [])
@@ -16,28 +16,8 @@ function Pagination({ toggleDislikeHandler, toggleLikeHandler, RemoveMovie }) {
     const moviesPerPage = 4;
     const pagesVisited = pageNumber * moviesPerPage;
 
-    const displayMovies = films
-        .slice(pagesVisited, pagesVisited + moviesPerPage)
-        .map((movie) => !activeCategory || activeCategory === movie.category ? (
-                <div className="movies-card">
-
-                    <div >
-                        <img className="movies_card_image" src="https://via.placeholder.com/400x250 " alt="" />
-                        <h4> {movie.title}</h4>
-                        <h4 id='category'>{movie.category}</h4>
-
-                    </div>
-                    <div className="movie_like_dislike">
-                        <span> <i class="fas fa-thumbs-up" onClick={toggleLikeHandler}></i>  {movie.likes} </span>
-                        <span> <i class="fas fa-thumbs-down" onClick={toggleDislikeHandler}></i>  {movie.dislikes}</span>
-                    </div>
-                    <div>
-                        <i style={{ cursor: 'pointer' }} className='fa fa-times-circle' onClick={() => RemoveMovie(movie.id)}></i>
-                    </div>
-                </div> ) : null
-            
-        );
-
+    const displayMovies = films.slice(pagesVisited, pagesVisited + moviesPerPage)
+    
     const pageCount = Math.ceil(films.length / moviesPerPage);
 
     const changePage = ({ selected }) => {
@@ -52,11 +32,31 @@ function Pagination({ toggleDislikeHandler, toggleLikeHandler, RemoveMovie }) {
                 AfficheMovies={AfficheMovies}
             />
             <div className='movies-list'>
-                {displayMovies}
+            {films.length > 0 ? (
+                displayMovies.map((movie , index) => !activeCategory || activeCategory === movie.category ? (
+            
+            <div className="movies-card" key={index}>
+
+                <div >
+                    <img className="movies_card_image" src="https://via.placeholder.com/400x250 " alt="" />
+                    <h4> {movie.title}</h4>
+                    <h4 id='category'>{movie.category}</h4>
+
+                </div>
+                <div className="movie_like_dislike">
+                    <span> <i class="fas fa-thumbs-up" onClick={toggleLikeHandler}></i>  {movie.likes} </span>
+                    <span> <i class="fas fa-thumbs-down" onClick={toggleDislikeHandler}></i> {movie.dislikes}  </span>
+                </div>
+                <div>
+                    <i style={{ cursor: 'pointer' }} className='fa fa-times-circle' onClick={() => RemoveMovie(index)}></i>
+                </div>
+            </div> ) : null
+        
+    )) : ( <div>Cart vide</div>)}
             
             <ReactPaginate
                 previousLabel={"Précédent"}
-                nextLabel={"Suivant ..."}
+                nextLabel={"Suivant.."}
                 pageCount={pageCount}
                 onPageChange={changePage}
                 containerClassName={"paginationBttns"}
